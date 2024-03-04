a833e836a2e6b3302a2a39d41e48a870c7908403
SpatikaPrusty <kspatika.prusty@lumiq.ai>
2024-03-04 11:18:10 +0530

    final changes in the server

--- models/users.js
+++ models/users.js
@@ -3,12 +3,31 @@ var bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema({
         name:{type: String, required: [true,'Please enter your name.']},
         age:{type: Number, required: [true,'Please enter your age.']},
-        gender:{type:String, required: [false]},
+        gender: {
+            type: String,
+            required: [false],
+            validate: {
+                validator: function(value) {
+                    return ['male', 'female'].includes(value.toLowerCase());
+                },
+                message: 'Gender must be either "male" or "female".'
+            }
+        },
         isSmoke:{type: Boolean, required: [true]},
         isDiabetic:{type: Boolean, required: [true]},
         incomePerAnnum: {type: Number, default: 0},
         mail: {type: String, required: [true]},
-        password: {type: String, required: [true]}
+        password: {
+            type: String,
+            required: [true],
+            validate: {
+                validator: function (value) {
+                    // Password must contain at least one digit
+                    return /\d/.test(value);
+                },
+                message: 'Password must contain at least one digit.'
+            }
+        }
     },
     {
         timestamps: true
